d9c85302fcf478cfd4078cb1d648f4b5e09f0639
elabob <elkabobenicova@gmail.com>
2025-07-12 18:42:20 +0200

    adding of about page and item page

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,10 @@
 from flask import Flask         #importuje triedu flask ktora sluzi na vyvtorenie webovej appky
 
 def create_app():       #vytvaram funkciu ktora mi umozni oddelit inicializaciu appky od spustenia
-    app = Flask(__name__)   #tymto vytvorim Flask aplikaciu
+    app = Flask(__name__, template_folder='templates')   #tymto vytvorim Flask aplikaciu
 
     from .routes import main  #nacitanie cesty z ineho suboru  #zaroven tu inicializujem blueprint- sposob ako organizovat cesty do samostatneho modulu
 
-    app.register_blueprint(main)  #pridavam subor main do aplikacie  #zaregistrujem blueprint do aplikacie aby Flask vedel ze su tam definovane trasy (/login,..)
+    app.register_blueprint(main, url_prefix='/')  #pridavam subor main do aplikacie  #zaregistrujem blueprint do aplikacie aby Flask vedel ze su tam definovane trasy (/login,..)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -15,3 +15,36 @@ def home_sk():
 @main.route('/en')
 def home_en():
     return render_template("home_en.html")
+
+@main.route('/cz')
+def home_cz():
+    return render_template("home_cz.html")
+
+
+#------------about section
+
+@main.route('/sk/about')
+def about_sk():
+    return render_template("about_sk.html")
+
+@main.route('/en/about')
+def about_en():
+    return render_template("about_en.html")
+
+@main.route('/cz/about')
+def about_cz():
+    return render_template("about_cz.html")
+
+#------------items section
+
+@main.route('/sk/items')
+def items_sk():
+    return render_template("items_sk.html")
+
+@main.route('/en/items')
+def items_en():
+    return render_template("items_en.html")
+
+@main.route('/cz/items')
+def items_cz():
+    return render_template("items_cz.html")
