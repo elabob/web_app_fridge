45854107da2dcf6586a8628764aac91feb18338a
elabob <elkabobenicova@gmail.com>
2025-07-12 18:00:02 +0200

    addition of English language option

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,14 @@ from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)   #vytvaram blueprint - maly modul aplikacie (main, auth, admin - kazdy so svojimi trasami)
 
-@main.route('/')    #ked niekto otvori adresu / spusti sa tato funkcia. Zobrazi stranku
-def home():
-    return render_template("home.html")     #zobrazi HTML sablonu z priecinka tamplates/
+@main.route('/')
+def index():
+    return render_template("home_sk.html")
+
+@main.route('/sk')    #ked niekto otvori adresu / spusti sa tato funkcia. Zobrazi stranku
+def home_sk():
+    return render_template("home_sk.html")     #zobrazi HTML sablonu z priecinka tamplates/
+
+@main.route('/en')
+def home_en():
+    return render_template("home_en.html")
